6349e8d58079e729517a7334ff1aae612afa0d77
Ricardo Mena C <rgacort12@gmail.com>
2024-04-27 15:13:12 -0600

    Tag base project
    Signed-off-by: Ricardo Mena C <rgacort12@gmail.com>

---
+++ src/hello_world/main.c
@@ -0,0 +1,18 @@
+/* 
+ * Ported to C language by Marcelo Samsoniuk
+ * based on BASIC code published by Satoshi Okue:
+ * https://twitter.com/S_Okue/status/1705175021000458592
+ * which appears to be based on:
+ * https://twitter.com/okazunori68/status/1629832782280282113
+ */
+
+#include <stdio.h>
+
+int main()
+{
+
+  printf("press <enter> to start...");
+        getchar();
+        printf("Hello World\n");
+        return 0;
+}
