672e10d0052ca8055b7fffcb27d4f85e75e30e04
ducrproofl0 <166961267+ducrproofl0@users.noreply.github.com>
2024-05-12 11:11:46 -0600

    Monitor 1: Connect monitor 1 + env + scoreboard

--- file_struc_edaplayground/testbench/env.sv
+++ file_struc_edaplayground/testbench/env.sv
@@ -1,19 +1,23 @@
 class environment;
   mem_driver mem_driver;
   stimulus sti;
+  scoreboard sb;
   monitor2 mntr2;
+  instr_monitor mntr1;
   
   function new();
     $display("Creating environment");
-    //sb = new();
+    sb = new();
     sti = new();
     mem_driver = new(sti);
     mntr2 = new();
+    mntr1 = new(sb);
     fork
       sti.mem_generate(1);
       mem_driver.mem_load();
       
       mntr2.check();
+      mntr1.check(0); //Debug-ability : No=0, Yes=1
     join_none
   endfunction
            
