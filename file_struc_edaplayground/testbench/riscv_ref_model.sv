f450baa65219647a40bf396ce94287e1512b0eeb
Sebastian Herrera Esquivel <64702743+cebherrera@users.noreply.github.com>
2024-05-13 08:50:42 -0600

    PC model update

--- file_struc_edaplayground/testbench/riscv_ref_model.sv
+++ file_struc_edaplayground/testbench/riscv_ref_model.sv
@@ -4,7 +4,7 @@ import instructions_data_struc::*;
 
 class riscv_ref_model;
   // Signals that store predicted value
-  //logic [31:0] pc_val_upd;
+  logic [31:0] pc_val_upd;
   logic [4:0] rs1_val_upd;
   logic [4:0] rs2_val_upd;
   logic [4:0] rdd_val_upd;
@@ -12,6 +12,9 @@ class riscv_ref_model;
   logic [31:0] DATAI;
   logic [31:0] DATAO;
   logic [31:0] DADDR;
+  logic [3:0] BE;
+  // For debug
+  logic [31:0] pc_val_in;
   // General Purpose 32x32 bit registers 
   logic [31:0] REGS [0:31];
   integer i;
@@ -36,99 +39,262 @@ class riscv_ref_model;
     $readmemh("darksocv.mem",MEM,0);
   endfunction
   
-  function predict(logic [7:0] rx_funct,logic signed [20:0] imm_val,logic [4:0] rs1_val,logic [4:0] rs2_val,logic [4:0] rdd_val);
-    // Procces and Execute Instruction, increment pc
+  function predict(logic [31:0] pc_val, logic [7:0] rx_funct,logic signed [20:0] imm_val,logic [4:0] rs1_val,logic [4:0] rs2_val,logic [4:0] rdd_val);
+    // L/S: DADDR[31] debe ser 0 para ejecutar esto, de lo contario se accede a I/O de los perifericos.
+    pc_val_in = pc_val; // Copy of the input PC for debug
     case (rx_funct)
       // R Type
-      ADD  : REGS[rdd_val] = REGS[rs1_val] + REGS[rs2_val];
-      SUB  : REGS[rdd_val] = REGS[rs1_val] - REGS[rs2_val];
-      XOR  : REGS[rdd_val] = REGS[rs1_val] ^ REGS[rs2_val];
-      OR   : REGS[rdd_val] = REGS[rs1_val] | REGS[rs2_val];
-      AND  : REGS[rdd_val] = REGS[rs1_val] & REGS[rs2_val];
-      SLL  : REGS[rdd_val] = REGS[rs1_val] << (REGS[rs2_val][4:0]);
-      SRL  : REGS[rdd_val] = REGS[rs1_val] >> (REGS[rs2_val][4:0]);
-      SRA  : REGS[rdd_val] = $signed(REGS[rs1_val]) >>> (REGS[rs2_val][4:0]); 
-      SLT  : REGS[rdd_val] = ($signed(REGS[rs1_val]) < $signed(REGS[rs2_val])) ? 1'b1 : 1'b0;
-      SLTU : REGS[rdd_val] = (REGS[rs1_val] < REGS[rs2_val]) ? 1'b1 : 1'b0;
+      ADD  : begin 
+        REGS[rdd_val] = REGS[rs1_val] + REGS[rs2_val];
+        pc_val = pc_val + 4;
+      end
+      SUB  : begin 
+        REGS[rdd_val] = REGS[rs1_val] - REGS[rs2_val];
+        pc_val = pc_val + 4;
+      end
+      XOR  : begin 
+        REGS[rdd_val] = REGS[rs1_val] ^ REGS[rs2_val];
+        pc_val = pc_val + 4;
+      end
+      OR   : begin 
+        REGS[rdd_val] = REGS[rs1_val] | REGS[rs2_val];
+        pc_val = pc_val + 4;
+      end
+      AND  : begin 
+        REGS[rdd_val] = REGS[rs1_val] & REGS[rs2_val];
+        pc_val = pc_val + 4;
+      end
+      SLL  : begin 
+        REGS[rdd_val] = REGS[rs1_val] << (REGS[rs2_val][4:0]);
+        pc_val = pc_val + 4;
+      end
+      SRL  : begin 
+        REGS[rdd_val] = REGS[rs1_val] >> (REGS[rs2_val][4:0]);
+        pc_val = pc_val + 4;
+      end
+      SRA  : begin 
+        REGS[rdd_val] = $signed(REGS[rs1_val]) >>> (REGS[rs2_val][4:0]); 
+        pc_val = pc_val + 4;
+      end
+      SLT  : begin 
+        REGS[rdd_val] = ($signed(REGS[rs1_val]) < $signed(REGS[rs2_val])) ? 1'b1 : 1'b0;
+        pc_val = pc_val + 4;
+      end
+      SLTU : begin 
+        REGS[rdd_val] = (REGS[rs1_val] < REGS[rs2_val]) ? 1'b1 : 1'b0;
+        pc_val = pc_val + 4;
+      end
       // I Type
-      ADDI : begin 
-        imm_val_sign_ext = {(imm_val[20])? '1 : '0, imm_val[20:0]}; 
-        REGS[rdd_val] = REGS[rs1_val] + imm_val_sign_ext;
+      ADDI : begin //This Operation is always signed
+        imm_val_sign_ext = {{11{imm_val[20]}}, imm_val[20:0]};  
+        REGS[rdd_val] = (REGS[rs1_val]) + (imm_val_sign_ext);
+        pc_val = pc_val + 4;
       end
       XORI : begin
-      	imm_val_sign_ext = {(imm_val[20])? '1 : '0, imm_val[20:0]}; 
+      	imm_val_sign_ext = {{11{imm_val[20]}}, imm_val[20:0]}; 
         REGS[rdd_val] = REGS[rs1_val] ^ imm_val_sign_ext;
+        pc_val = pc_val + 4;
       end
       ORI  : begin
-        imm_val_sign_ext = {(imm_val[20])? '1 : '0, imm_val[20:0]}; 
+        imm_val_sign_ext = {{11{imm_val[20]}}, imm_val[20:0]};  
         REGS[rdd_val] = REGS[rs1_val] | imm_val_sign_ext;
+        pc_val = pc_val + 4;
       end
       ANDI : begin
-        imm_val_sign_ext = {(imm_val[20])? '1 : '0, imm_val[20:0]}; 
+        imm_val_sign_ext = {{11{imm_val[20]}}, imm_val[20:0]};  
       	REGS[rdd_val] = REGS[rs1_val] & imm_val_sign_ext;
+        pc_val = pc_val + 4;
       end
       SLLI : begin
-        imm_val_sign_ext = {(imm_val[20])? '1 : '0, imm_val[20:0]};
+        imm_val_sign_ext = {{11{imm_val[20]}}, imm_val[20:0]}; 
         REGS[rdd_val] = REGS[rs1_val] << (imm_val_sign_ext[4:0]);
+        pc_val = pc_val + 4;
       end
       SRLI : begin 
-        imm_val_sign_ext = {(imm_val[20])? '1 : '0, imm_val[20:0]};
-        REGS[rdd_val] = REGS[rs1_val] >> (imm_val_sign_ext[4:0]);
+        imm_val_sign_ext = {{11{imm_val[20]}}, imm_val[20:0]}; 
+        REGS[rdd_val] = REGS[rs1_val] >> (imm_val_sign_ext[4:0]); 
+        pc_val = pc_val + 4;
       end
       SRAI : begin 
-        imm_val_sign_ext = {(imm_val[20])? '1 : '0, imm_val[20:0]};
+        imm_val_sign_ext = {{11{imm_val[20]}}, imm_val[20:0]}; 
         REGS[rdd_val] = $signed(REGS[rs1_val]) >>> (imm_val_sign_ext[4:0]);
+        pc_val = pc_val + 4;
       end
       SLTI : begin
-        imm_val_sign_ext = {(imm_val[20])? '1 : '0, imm_val[20:0]};
-        REGS[rdd_val] = ($signed(REGS[rs1_val]) < imm_val_sign_ext) ? 1'b1 : 1'b0;
+        imm_val_sign_ext = {{11{imm_val[20]}}, imm_val[20:0]}; 
+        REGS[rdd_val] = ($signed(REGS[rs1_val]) < imm_val_sign_ext) ? 1'b1 : 1'b0; 
+        pc_val = pc_val + 4;
       end
       SLTIU: begin 
-        imm_val_sign_ext = {(imm_val[20])? '1 : '0, imm_val[20:0]};
+        imm_val_sign_ext = {{11{imm_val[20]}}, imm_val[20:0]}; 
         REGS[rdd_val] = (REGS[rs1_val] < $unsigned(imm_val_sign_ext)) ? 1'b1 : 1'b0;
+        pc_val = pc_val + 4;
       end
-      /*
       // I-L(load)Type
       LB   : begin
-      	imm_val_sign_ext = {(imm_val[20])? '1 : '0, imm_val[20:0]};
+        imm_val_sign_ext = {{11{imm_val[20]}}, imm_val[20:0]}; 
         DADDR = REGS[rs1_val] + imm_val_sign_ext;
-        DATAI = MEM[DADDR];
-        REGS[rdd_val] = DATAI[7:0];
-      end 
+        DATAI = MEM[DADDR[`MLEN-1:2]]; 
+        case (DADDR[1:0])
+          3: BE = 4'b1000;
+          2: BE = 4'b0100;
+          1: BE = 4'b0010;
+          0: BE = 4'b0001;
+        endcase
+        case (BE)
+          4'b1000: DATAI = {{24{DATAI[31]}},DATAI[31:24]};
+          4'b0100: DATAI = {{24{DATAI[23]}},DATAI[23:16]};
+          4'b0010: DATAI = {{24{DATAI[15]}},DATAI[15:8 ]};
+          4'b0001: DATAI = {{24{DATAI[7 ]}},DATAI[7:0  ]};
+        endcase
+        REGS[rdd_val] = DATAI;
+        pc_val = pc_val + 4;
+      end
       LH   : begin
-        imm_val_sign_ext = {(imm_val[20])? '1 : '0, imm_val[20:0]};
+      	imm_val_sign_ext = {{11{imm_val[20]}}, imm_val[20:0]}; 
+        DADDR = REGS[rs1_val] + imm_val_sign_ext;
+        DATAI = MEM[DADDR[`MLEN-1:2]];
+        case (DADDR[0])
+          1: BE = 4'b1100;
+          0: BE = 4'b0011;
+        endcase
+        case (BE)
+          4'b1100: DATAI = {{16{DATAI[31]}},DATAI[31:16]};
+          4'b0011: DATAI = {{16{DATAI[15]}},DATAI[15:0]};
+        endcase
+        REGS[rdd_val] = DATAI;
+        pc_val = pc_val + 4;
       end
       LW   : begin 
-        imm_val_sign_ext = {(imm_val[20])? '1 : '0, imm_val[20:0]};
+        imm_val_sign_ext = {{11{imm_val[20]}}, imm_val[20:0]};
+        DADDR = REGS[rs1_val] + imm_val_sign_ext;
+        DATAI = MEM[DADDR[`MLEN-1:2]];
+        BE = 4'b1111;
+        REGS[rdd_val] = DATAI;
+        pc_val = pc_val + 4;
       end
       LBU  : begin 
-        imm_val_sign_ext = {(imm_val[20])? '1 : '0, imm_val[20:0]};
+      	imm_val_sign_ext = {{11{imm_val[20]}}, imm_val[20:0]}; 
+        DADDR = REGS[rs1_val] + imm_val_sign_ext;
+        DATAI = MEM[DADDR[`MLEN-1:2]]; 
+        case (DADDR[1:0])
+          3: BE = 4'b1000;
+          2: BE = 4'b0100;
+          1: BE = 4'b0010;
+          0: BE = 4'b0001;
+        endcase
+        case (BE)
+          4'b1000: DATAI = {{24{1'b0}},DATAI[31:24]};
+          4'b0100: DATAI = {{24{1'b0}},DATAI[23:16]};
+          4'b0010: DATAI = {{24{1'b0}},DATAI[15:8]};
+          4'b0001: DATAI = {{24{1'b0}},DATAI[7:0]};
+        endcase
+        REGS[rdd_val] = DATAI;
+        pc_val = pc_val + 4;
       end
       LHU  : begin 
-        imm_val_sign_ext = {(imm_val[20])? '1 : '0, imm_val[20:0]};
+      	imm_val_sign_ext = {{11{imm_val[20]}}, imm_val[20:0]}; 
+        DADDR = REGS[rs1_val] + imm_val_sign_ext;
+        DATAI = MEM[DADDR[`MLEN-1:2]];
+        case (DADDR[0])
+          1: BE = 4'b1100;
+          0: BE = 4'b0011;
+        endcase
+        case (BE)
+          4'b1100: DATAI = {{16{1'b0}},DATAI[31:16]};
+          4'b0011: DATAI = {{16{1'b0}},DATAI[15:0]};
+        endcase
+        REGS[rdd_val] = DATAI;
+        pc_val = pc_val + 4;
       end 
       // S-Type
-      SB   : imm_val_sign_ext = {(imm_val[20])? '1 : '0, imm_val[20:0]};
-	  SH   : imm_val_sign_ext = {(imm_val[20])? '1 : '0, imm_val[20:0]};
-      SW   : imm_val_sign_ext = {(imm_val[20])? '1 : '0, imm_val[20:0]};
+      SB   : begin 
+        imm_val_sign_ext = {{11{imm_val[20]}}, imm_val[20:0]};
+        DADDR = REGS[rs1_val] + imm_val_sign_ext;
+        case (DADDR[1:0])
+          3: BE = 4'b1000;
+          2: BE = 4'b0100;
+          1: BE = 4'b0010;
+          0: BE = 4'b0001;
+        endcase
+        case (BE)
+          4'b1000: begin 
+            DATAO = {REGS[rs2_val][7:0],{24{1'b0}}};
+            MEM[DADDR[`MLEN-1:2]][31:24] = DATAO[31:24];
+          end 
+          4'b0100: begin
+            DATAO = {{8{1'b0}},REGS[rs2_val][7:0],{16{1'b0}}};
+            MEM[DADDR[`MLEN-1:2]][23:16] = DATAO[23:16];
+          end
+          4'b0010: begin 
+            DATAO = {{16{1'b0}},REGS[rs2_val][7:0],{8{1'b0}}};
+            MEM[DADDR[`MLEN-1:2]][15:8] = DATAO[15:8];
+          end
+          4'b0001: begin 
+            DATAO = {{24{1'b0}},REGS[rs2_val][7:0]};
+            MEM[DADDR[`MLEN-1:2]][7:0] = DATAO[7:0];
+          end 
+        endcase
+        pc_val = pc_val + 4;
+      end
+	  SH   : begin 
+        imm_val_sign_ext = {{11{imm_val[20]}}, imm_val[20:0]};
+        DADDR = REGS[rs1_val] + imm_val_sign_ext;
+        case (DADDR[0])
+          1: BE = 4'b1100;
+          0: BE = 4'b0011;
+        endcase
+        case (BE)
+          4'b1100: begin 
+            DATAO = {REGS[rs2_val][15:0],{16{1'b0}}};
+            MEM[DADDR[`MLEN-1:2]][31:16] = DATAO[31:16];
+          end
+          4'b0011: begin 
+            DATAO = {{16{1'b0}},REGS[rs2_val][15:0]};
+            MEM[DADDR[`MLEN-1:2]][15:0] = DATAO[15:0];
+          end 
+        endcase
+        pc_val = pc_val + 4;
+      end
+      SW   : begin 
+        imm_val_sign_ext = {{11{imm_val[20]}}, imm_val[20:0]};
+        DADDR = REGS[rs1_val] + imm_val_sign_ext;
+        DATAO = REGS[rs2_val];
+        BE = 4'b1111;
+        MEM[DADDR[`MLEN-1:2]] = DATAO;
+        pc_val = pc_val + 4;
+      end
+      /*
       // S-B-Type
-      BEQ  : imm_val_sign_ext = {(imm_val[20])? '1 : '0, imm_val[20:0]};
-      BNE  : imm_val_sign_ext = {(imm_val[20])? '1 : '0, imm_val[20:0]};
-      BLT  : imm_val_sign_ext = {(imm_val[20])? '1 : '0, imm_val[20:0]};
-      BGE  : imm_val_sign_ext = {(imm_val[20])? '1 : '0, imm_val[20:0]};
-      BLTU : imm_val_sign_ext = {(imm_val[20])? '1 : '0, imm_val[20:0]}; 
-      BGEU : imm_val_sign_ext = {(imm_val[20])? '1 : '0, imm_val[20:0]}; 
-      // J-Type
-      JAL  : imm_val_sign_ext = {(imm_val[20])? '1 : '0, imm_val[20:0]};
-      JALR : imm_val_sign_ext = {(imm_val[20])? '1 : '0, imm_val[20:0]};
-      // U-Type
-      LUI  : imm_val_sign_ext = {imm_val[20:0], '0};
-	  AUIPC: imm_val_sign_ext = {imm_val[20:0], '0};
+      BEQ  : imm_val_sign_ext = {{11{imm_val[20]}}, imm_val[20:0]}; 
+      BNE  : imm_val_sign_ext = {{11{imm_val[20]}}, imm_val[20:0]}; 
+      BLT  : imm_val_sign_ext = {{11{imm_val[20]}}, imm_val[20:0]}; 
+      BGE  : imm_val_sign_ext = {{11{imm_val[20]}}, imm_val[20:0]}; 
+      BLTU : imm_val_sign_ext = {{11{imm_val[20]}}, imm_val[20:0]};  
+      BGEU : imm_val_sign_ext = {{11{imm_val[20]}}, imm_val[20:0]};  
       */
+      // J-Type // JAL with rd = 0x is a plian jump
+      JAL  : begin // max jump ±1 MiB range based on the extended sign imm 
+      	imm_val_sign_ext = {{11{imm_val[20]}}, imm_val[20:0]}; 
+        REGS[rdd_val] = pc_val+4;
+        pc_val = pc_val + imm_val_sign_ext; // LSB = 0? I think the two LSB should be 0 to align bytes, wire [31:0] JVAL = JALR ? DADDR : PCSIMM; // SIMM + (JALR ? U1REG : PC);
+      end 
+      JALR : begin 
+        imm_val_sign_ext = {{11{imm_val[20]}}, imm_val[20:0]}; 
+        REGS[rdd_val] = pc_val+4;
+        pc_val = REGS[rs1_val] + imm_val_sign_ext;
+      end
+      // U-Type
+      LUI  : begin 
+        imm_val_sign_ext = {imm_val[20:0], '0}; //rd = (sll) imm << 12 or {imm,,{12{1'b0}}} 
+      end 
+      AUIPC: begin 
+        imm_val_sign_ext = {imm_val[20:0], '0}; //rd = PC + (imm << 12), 12 lower bits to 0
+      end
     endcase
     
     // Update Predicted Values
-    //pc_val_upd  = pc_val;
+    pc_val_upd  = pc_val;
     rs1_val_upd = rs1_val;
     rs2_val_upd = rs2_val;
     rdd_val_upd = rdd_val;
