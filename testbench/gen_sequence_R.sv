46a080fc7df2881d1474257373f11696aa06f4dc
dchavarrias <dennis.chavarriasoto@ucr.ac.cr>
2024-07-12 20:40:40 -0600

    Esto randomiza, estructura de while básica necesita a Jesus

--- testbench/gen_sequence_R.sv
+++ testbench/gen_sequence_R.sv
@@ -6,26 +6,62 @@ class gen_sequence_R extends gen_sequence;
         super.new(name);
     endfunction
 
+    //rand logic [3:0] num_chunks; //Cuantos grupos de 6 instrucciones se usaran?
+
+    logic [6:0] total_chunks;  //Cuantos grupos de 6 instrucciones hay disponibles
+    logic [6:0] puntero_chunk; //Por cual grupo de 6 instrucciones vamos?
+    logic       bndra_ncsto_inc; //Bandera necesito incremento
+    logic [8:0] total_nops;  //Numero total de nops por branch
+
     virtual task body();
         sequence_item_rv32i_instruction item_0 = sequence_item_rv32i_instruction::type_id::create("item_0"); // Instruction i
-
+        branch_aux_vars aux_vars = branch_aux_vars::type_id::create("aux_vars"); // Instruction i
+        total_chunks = 80; // For defecto vamos a generar unos 80 chunks
+        puntero_chunk = 85;
         //********* Inicio de generacion de secuencia (Programa para arquitectura rv32i) ***********
         //`uvm_info("SEQUENCE", $sformatf("Generate instructions:"), UVM_MEDIUM)
-        // Para la cantidad de instrucciones correspondiente (512 actualmente)
-        for(int i=0; i < 2**`MLEN/(4*2); i=i+1) begin 
-
-            //*** Seteando registros (instrucciones 0-30)
-            if (i <= 30) begin
-                item_0.randomize() with {opcode==I_TYPE && funct3==ADDI_FC && rs1==0 && rd==i+1;};
-                //  todo: meter LUI para setear parte alta de registros y no solo primeros 12 bits
-                //Transaccion
-                start_item(item_0);
-                finish_item(item_0);
-                $display("\n(ADDI for set regs)\tInstruct #%d\t\tInstruction :%h\t", i[15:0], item_0.full_inst);  
+        // Para la cantidad de instrucciones correspondiente (512 actualmente) 
+      
+        while ( (total_chunks>15)&&(puntero_chunk>0) )begin
+            //Aqui genera variables que son generales a todos los tipos de branch
+            aux_vars.randomize(); //Genera el tipo de branch y cuantos chunks
+            total_nops = (aux_vars.num_chunks*6)-5;
+            $display("Necesito esta cantidad de chunks para este branch%d", aux_vars.num_chunks);
+            $display("Para este branch necesito esta cantidad de nops %d", total_nops);
+            $display("Chunks restantes: %d", total_chunks);
+            //Calcular el inmediat del jal.
+            //Calcular el inmediat del branch.
+            //Usar los punteros de registros rs1 y rs2 de forma que tenga sentido.
+            //Caso BEQ
+            if (1)begin //Esto debería ser aux_vars.tipo_branch == loquesea.
+                $display("Vamos a generar un BEQ");
+                //Creadores de estado de condicion
+                item_0.randomize() with {opcode==I_TYPE && funct3==ADDI_FC && rs1==0 && rd==0;};
+                item_0.randomize() with {opcode==I_TYPE && funct3==ADDI_FC && rs1==1 && rd==0;};
+                //Branch
+                //item_0.randomize() crear branch
+                //Cuerpo de nops
+                for(int k=0; k < total_nops; k=k+1) begin
+                    $display("Generando nop numero %d", k);
+                    item_0.randomize() with {opcode==I_TYPE && funct3==ADDI_FC && rs1==1 && rd==0;};
+                end
+                //Ruptura ADD-Condicionarlo, si rs1_val es mayor que rs2 de acuerdo con el algoritmo
+                //If rs1 mayor que rs2 usar decremento.
+                //If rs2 mayor que rs2 usar incremento.
+                item_0.randomize() with {opcode==I_TYPE && funct3==ADDI_FC && rs1==1 && rd==0;};
+                //JALR o JAL de regreso
+                //  item 0
             end
-            
+
+            //Fin de la secuencia
+            total_chunks = total_chunks-aux_vars.num_chunks;
+            puntero_chunk = puntero_chunk - 1;
+        end
+
+
+        for(int i=0; i < 10; i=i+1) begin             
             // Cuando llegue la ultima instruccion, meter jal para retroceder
-            else if ( i == 2**`MLEN/(4*2) - 1 ) begin
+            if ( i == 9 ) begin
                 item_0.randomize() with {opcode==J_TYPE && imm_jal[20:10]==11'hfff ;};       
                 $display("\n(for JAL)\t\tInstruct #%d\t\tinstruct: %h\tOffset: %b   (bin)", i[15:0], item_0.full_inst, item_0.imm_jal);
                 // Transaccion JAL
--- testbench/sequence_item_rv32i_instruction.sv
+++ testbench/sequence_item_rv32i_instruction.sv
@@ -201,5 +201,30 @@ class sequence_item_rv32i_instruction extends uvm_sequence_item;
 
 endclass
 
+class branch_aux_vars extends uvm_sequence_item; //TODO Revisar si se puede usar algo diferente a sequence.
+  function new(string name = "branch_aux_vars");
+    super.new(name);
+  endfunction
+
+  // random variables 
+  rand logic [4:0] num_chunks; //Cuantos grupos de 6 instrucciones se usaran?
+  rand logic [2:0] tipo_branch; //Para generar uno de los 6 valores disponibles. 
+  rand logic [31:0] rs1_beq_val;
+  rand logic [31:0] rs2_beq_val;
+
+  rand logic [4:0] rs1_ptr;
+  rand logic [4:0] rs2_ptr;
+  
+
+  constraint valid_branches {6>tipo_branch; 0<=tipo_branch;}
+  constraint valid_rng_num_chunks {30>num_chunks; 1<=num_chunks;}
+
+//  constraint valid_rs1_beq_val {1000>rs1_beq_val; -1000<=rs1_beq_val;}
+//  constraint valid_rs2_beq_val {1000>rs2_beq_val; -1000<=rs1_beq_val;}  
+
+  //*******************************************************
+  `uvm_object_utils(branch_aux_vars)
+endclass
+
 //Excellent reference for conditional constraints:
 //  https://vlsiverify.com/system-verilog/if-else-in-constraints/
