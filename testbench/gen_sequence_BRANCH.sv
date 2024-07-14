1a53d49ec577d28a2514b6147c05a706499adb38
jjimenezu <eddy.jimenezulate@ucr.ac.cr>
2024-07-13 22:59:48 -0600

    Ready BNE,BLT,BGE generation

--- testbench/gen_sequence_BRANCH.sv
+++ testbench/gen_sequence_BRANCH.sv
@@ -13,9 +13,10 @@ class gen_sequence_BRANCH extends gen_sequence;
     //logic       bndra_ncsto_inc; //Bandera necesito incremento
     
     int instruction_counter = 0;
-    logic [8:0] total_nops;  //Numero total de nops por branch
-    logic [31:0] addi2_imm_val;
-    logic signed [20:1] offset_jump;
+    logic [8:0] total_nops;             // Numero total de nops por branch
+    logic [31:0] addi2_imm_val;         // Valor inmediato para ADDI_2
+    logic signed [20:1] offset_jump;    // Para calcular el offset negativo 
+    logic [11:0] inc_dec_value;         // Valor inmediato de incremento/decremento para ADDI_rupt
     
     virtual task body();
         
@@ -51,16 +52,30 @@ class gen_sequence_BRANCH extends gen_sequence;
                 //*** Generacion de instrucciones BRANCH y JAL con su respectivo offset ***//
                 total_nops   = $urandom_range(5,20);   //($urandom_range(1,29) * 6) - 5
                 offset_jump = -(1+total_nops+1)*(4/2);
-                item_BRANCH.randomize() with {opcode==S_B_TYPE && funct3==BEQ_FC && rs1!=0 && rs2!=0 && rs1!=rs2 && imm==(total_nops+1+1+1)*(4/2);};  //offset = nops + addi_rupt + jal + 1   //&& imm==(total_nops+1+1+1)*4
+                item_BRANCH.randomize() with {opcode==S_B_TYPE && funct3==BGE_FC && rs1!=0 && rs2!=0 && rs1!=rs2 && imm==(total_nops+1+1+1)*(4/2);};  //offset = nops + addi_rupt + jal + 1   //&& imm==(total_nops+1+1+1)*4
                 item_JAL.randomize() with {opcode==J_TYPE && rd==0 && imm_jal==offset_jump;};
 
-                //*** Enviar creadores de estado de condicion ADDI_1 y ADDI_2 ***//
-                item_ADDI_1.randomize() with {opcode==I_TYPE && funct3==ADDI_FC && rs1==0 && rd==item_BRANCH.rs1;};
-                addi2_imm_val = $urandom_range(item_ADDI_1.imm-10 , item_ADDI_1.imm+10); // Asegurar que no hay mucha diferencia entre los valores para que no se genere un loop excesivo
-                item_ADDI_2.randomize() with {opcode==I_TYPE && funct3==ADDI_FC && rs1==0 && rd==item_BRANCH.rs2 && imm==addi2_imm_val;}; //todo: Este randomize debe ser diferente para las BNE. Implementar mediante un if
+                //*** Enviar creadores de estado de condicion ADDI_1 y ADDI_2. Dependen del tipo de BRANCH ***//
+                item_ADDI_1.randomize() with {opcode==I_TYPE && funct3==ADDI_FC && rs1==0 && rd==item_BRANCH.rs1;}; // ADDI_1 es el mismo siempre
+                // BNE
+                if (item_BRANCH.funct3 == BNE_FC) begin
+                    addi2_imm_val = item_ADDI_1.imm; //imm de ADDI_1 igual que imm de ADDI_2 para el caso de BNE 
+                // BEQ
+                end else if (item_BRANCH.funct3 == BEQ_FC) begin
+                    do
+                        addi2_imm_val = $urandom_range(item_ADDI_1.imm-10 , item_ADDI_1.imm+10); // Asegurar que no hay mucha diferencia entre los valores para que no se genere un loop excesivo
+                    while (addi2_imm_val == item_ADDI_1.imm);  // Asegurar que los valores asignados por los ADDI 1 y 2 sean diferentes     
+                // BLT
+                end else if (item_BRANCH.funct3 == BLT_FC) begin
+                    addi2_imm_val = $urandom_range(item_ADDI_1.imm-10 , item_ADDI_1.imm-1); // Asegurar que rs1_val > rs2_val para que no se tome el salto inicialmente
+                // BGE
+                end else if (item_BRANCH.funct3 == BGE_FC) begin
+                    addi2_imm_val = $urandom_range(item_ADDI_1.imm+1 , item_ADDI_1.imm+10); // Asegurar que rs1_val < rs2_val para que no se tome el salto inicialmente
+                end
+                item_ADDI_2.randomize() with {opcode==I_TYPE && funct3==ADDI_FC && rs1==0 && rd==item_BRANCH.rs2 && imm==addi2_imm_val;};
                 // Info
-                $display("\n(ADDI_1)\t\t\tPC: 0x%h\t\tInstruction: %h\t", instruction_counter[9:0]*4, item_ADDI_1.full_inst);
-                $display("(ADDI_2)\t\t\tPC: 0x%h\t\tInstruction: %h\t", instruction_counter[9:0]*4+1'b1, item_ADDI_2.full_inst);
+                $display("\n(ADDI_1)\t\t\tPC: 0x%h\t\tInstruction: %h\timm: 0x%h", instruction_counter[9:0]*4, item_ADDI_1.full_inst, item_ADDI_1.imm);
+                $display("(ADDI_2)\t\t\tPC: 0x%h\t\tInstruction: %h\timm: 0x%h", instruction_counter[9:0]*4+1'b1, item_ADDI_2.full_inst, item_ADDI_2.imm);
                 // Transactions
                 start_item(item_ADDI_1);
                 finish_item(item_ADDI_1);
@@ -69,9 +84,9 @@ class gen_sequence_BRANCH extends gen_sequence;
                 instruction_counter+=2;
 
                 //*** Enviar Branch ***//
-                // Transaction
                 // Info
                 $display("(BRANCH)\t\t\tPC: 0x%h\t\tInstruction: %h\toffset: 0x%h\t\tTarget_PC: 0x%h", instruction_counter[9:0]*4, item_BRANCH.full_inst, $signed(item_BRANCH.imm)*2'h2, instruction_counter*4+$signed(item_BRANCH.imm)*2);
+                // Transaction
                 start_item(item_BRANCH);
                 finish_item(item_BRANCH);
                 instruction_counter++;
@@ -86,26 +101,36 @@ class gen_sequence_BRANCH extends gen_sequence;
                     instruction_counter++;         
                 end
 
-
-                 //*** Enviar ADDI para ruptura de condicion, dependiendo del tipo de branch ***//
-                if (item_BRANCH.funct3 == BEQ_FC) begin     //Caso BEQ
-                
-                    // If rs1_value > que rs2_value : enviar instruccion que decremente rs1_value
+                //*** Enviar ADDI para ruptura de condicion, dependiendo del tipo de branch ***//
+                //Caso BEQ: incrementar/decrementar de 1 en 1.
+                if ( (item_BRANCH.funct3 == BEQ_FC) ) begin     
                     if ( item_ADDI_1.imm > item_ADDI_2.imm) begin
-                        item_ADDI_rupt.randomize() with {opcode==I_TYPE && funct3==ADDI_FC && rs1==item_BRANCH.rs1 && rd==item_BRANCH.rs1 && imm==12'hfff;};
+                        inc_dec_value = 12'hfff;
                     end
-                    // If rs1_value < que rs2_value : enviar instruccion que incremente rs1_value 
                     else if (item_ADDI_1.imm < item_ADDI_2.imm) begin
-                        item_ADDI_rupt.randomize() with {opcode==I_TYPE && funct3==ADDI_FC && rs1==item_BRANCH.rs1 && rd==item_BRANCH.rs1 && imm==12'h001;};
+                        inc_dec_value = 12'h001;
                     end
-                    // Info
-                    $display("(ADDI_rupt)\t\tPC: 0x%h\t\tInstruction: %h\timm:", instruction_counter[9:0]*4, item_ADDI_rupt.full_inst, $signed(item_ADDI_rupt.imm));
-                    // Transaction
-                    start_item(item_ADDI_rupt);
-                    finish_item(item_ADDI_rupt);
-                    instruction_counter++;
                 end
-                //TODO: condicionales para las condiciones de ruptura de los restantes tipos de branch
+                // Caso BNE: incrementar/decrementar por cualquier valor excepto por 0
+                else if (item_BRANCH.funct3 == BNE_FC) begin
+                    inc_dec_value = $urandom_range(1,4095);
+                end
+                // Caso BLT || BGE: incrementar/decrementar de 2 en 2 para que sea posible ejercitar mejor BGE (caso "mayor que" y caso "igual que")
+                else if ((item_BRANCH.funct3 == BLT_FC) || (item_BRANCH.funct3 == BGE_FC)) begin
+                    if ( item_ADDI_1.imm > item_ADDI_2.imm) begin //BLT
+                        inc_dec_value = 12'hffe;
+                    end
+                    else if (item_ADDI_1.imm < item_ADDI_2.imm) begin //BGE
+                        inc_dec_value = 12'h002;
+                    end
+                end
+                item_ADDI_rupt.randomize() with {opcode==I_TYPE && funct3==ADDI_FC && rs1==item_BRANCH.rs1 && rd==item_BRANCH.rs1 && imm==inc_dec_value;};   
+                // Info
+                $display("(ADDI_rupt)\t\tPC: 0x%h\t\tInstruction: %h\timm:", instruction_counter[9:0]*4, item_ADDI_rupt.full_inst, $signed(item_ADDI_rupt.imm));
+                // Transaction
+                start_item(item_ADDI_rupt);
+                finish_item(item_ADDI_rupt);
+                instruction_counter++;
 
                 //*** Enviar JAL para generar loop ***//
                 // Info
