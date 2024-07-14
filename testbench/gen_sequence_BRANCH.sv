9e45b595c42ce11f09306c8bec91878fd374851d
jjimenezu <eddy.jimenezulate@ucr.ac.cr>
2024-07-14 17:13:55 -0600

    All branches enabled

--- testbench/gen_sequence_BRANCH.sv
+++ testbench/gen_sequence_BRANCH.sv
@@ -41,18 +41,11 @@ class gen_sequence_BRANCH extends gen_sequence;
            
             // Generacion de estructuras de branches "chunks"
             if ( instruction_counter <= 2**`MLEN/(4*2) - 30) begin
-                //Aqui genera variables que son generales a tods los tipos de branch   
-                //aux_vars.randomize(); //Genera cuantos chunks          
-                //total_nops = (aux_vars.num_chunks*6)-5;
-                //$display("Necesito esta cantidad de chunks para este branch%d", (total_nops+5)/6 );  //aux_vars.num_chunks
-                //$display("Para este branch necesito esta cantidad de nops %d", total_nops);
-                //$display("Chunks restantes: %d", total_chunks);
-                //Usar los punteros de registros rs1 y rs2 de forma que tenga sentido.
                 
                 //*** Generacion de instrucciones BRANCH y JAL con su respectivo offset ***//
                 total_nops   = $urandom_range(5,20);   //($urandom_range(1,29) * 6) - 5
                 offset_jump = -(1+total_nops+1)*(4/2);
-                item_BRANCH.randomize() with {opcode==S_B_TYPE && funct3==BGE_FC && rs1!=0 && rs2!=0 && rs1!=rs2 && imm==(total_nops+1+1+1)*(4/2);};  //offset = nops + addi_rupt + jal + 1   //&& imm==(total_nops+1+1+1)*4
+                item_BRANCH.randomize() with {opcode==S_B_TYPE && rs1!=0 && rs2!=0 && rs1!=rs2 && imm==(total_nops+1+1+1)*(4/2);};  //offset = nops + addi_rupt + jal + 1   //&& imm==(total_nops+1+1+1)*4
                 item_JAL.randomize() with {opcode==J_TYPE && rd==0 && imm_jal==offset_jump;};
 
                 //*** Enviar creadores de estado de condicion ADDI_1 y ADDI_2. Dependen del tipo de BRANCH ***//
@@ -66,16 +59,16 @@ class gen_sequence_BRANCH extends gen_sequence;
                         addi2_imm_val = $urandom_range(item_ADDI_1.imm-10 , item_ADDI_1.imm+10); // Asegurar que no hay mucha diferencia entre los valores para que no se genere un loop excesivo
                     while (addi2_imm_val == item_ADDI_1.imm);  // Asegurar que los valores asignados por los ADDI 1 y 2 sean diferentes     
                 // BLT
-                end else if (item_BRANCH.funct3 == BLT_FC) begin
+                end else if ( (item_BRANCH.funct3 == BLT_FC) || (item_BRANCH.funct3 == BLTU_FC) ) begin
                     addi2_imm_val = $urandom_range(item_ADDI_1.imm-10 , item_ADDI_1.imm-1); // Asegurar que rs1_val > rs2_val para que no se tome el salto inicialmente
                 // BGE
-                end else if (item_BRANCH.funct3 == BGE_FC) begin
+                end else if ((item_BRANCH.funct3 == BGE_FC) || (item_BRANCH.funct3 == BGEU_FC)) begin
                     addi2_imm_val = $urandom_range(item_ADDI_1.imm+1 , item_ADDI_1.imm+10); // Asegurar que rs1_val < rs2_val para que no se tome el salto inicialmente
                 end
                 item_ADDI_2.randomize() with {opcode==I_TYPE && funct3==ADDI_FC && rs1==0 && rd==item_BRANCH.rs2 && imm==addi2_imm_val;};
                 // Info
-                $display("\n(ADDI_1)\t\t\tPC: 0x%h\t\tInstruction: %h\timm: 0x%h", instruction_counter[9:0]*4, item_ADDI_1.full_inst, item_ADDI_1.imm);
-                $display("(ADDI_2)\t\t\tPC: 0x%h\t\tInstruction: %h\timm: 0x%h", instruction_counter[9:0]*4+1'b1, item_ADDI_2.full_inst, item_ADDI_2.imm);
+                $display("\n(ADDI_1)\t\tPC: 0x%h\t\tInstruction: %h\timm: 0x%h", instruction_counter[9:0]*4, item_ADDI_1.full_inst, item_ADDI_1.imm);
+                $display("(ADDI_2)\t\tPC: 0x%h\t\tInstruction: %h\timm: 0x%h", instruction_counter[9:0]*4+1'b1, item_ADDI_2.full_inst, item_ADDI_2.imm);
                 // Transactions
                 start_item(item_ADDI_1);
                 finish_item(item_ADDI_1);
@@ -85,7 +78,7 @@ class gen_sequence_BRANCH extends gen_sequence;
 
                 //*** Enviar Branch ***//
                 // Info
-                $display("(BRANCH)\t\t\tPC: 0x%h\t\tInstruction: %h\toffset: 0x%h\t\tTarget_PC: 0x%h", instruction_counter[9:0]*4, item_BRANCH.full_inst, $signed(item_BRANCH.imm)*2'h2, instruction_counter*4+$signed(item_BRANCH.imm)*2);
+                $display("(BRANCH)\t\tPC: 0x%h\t\tInstruction: %h\toffset: 0x%h\t\tTarget_PC: 0x%h", instruction_counter[9:0]*4, item_BRANCH.full_inst, $signed(item_BRANCH.imm)*2'h2, instruction_counter*4+$signed(item_BRANCH.imm)*2);
                 // Transaction
                 start_item(item_BRANCH);
                 finish_item(item_BRANCH);
@@ -121,6 +114,15 @@ class gen_sequence_BRANCH extends gen_sequence;
                         inc_dec_value = 12'hffe;
                     end
                     else if (item_ADDI_1.imm < item_ADDI_2.imm) begin //BGE
+                        inc_dec_value = 12'h002;
+                    end
+                end
+                // Caso BLTU || BGEU
+                else if ( (item_BRANCH.funct3 == BLTU_FC) || (item_BRANCH.funct3 == BGEU_FC) ) begin
+                    if ( {1'b0,item_ADDI_1.imm} > {1'b0,item_ADDI_2.imm} ) begin //BLTU
+                        inc_dec_value = 12'hffe;
+                    end
+                    else if ({1'b0,item_ADDI_1.imm} < {1'b0,item_ADDI_2.imm}) begin //BGEU
                         inc_dec_value = 12'h002;
                     end
                 end
@@ -163,35 +165,5 @@ class gen_sequence_BRANCH extends gen_sequence;
                 instruction_counter++;
             end
         end
-
-
-
-        //     //Fin de la secuencia
-        //     total_chunks = total_chunks-aux_vars.num_chunks;
-        //     puntero_chunk = puntero_chunk - 1;
-        // end
-
-
-        // for(int i=0; i < 10; i=i+1) begin             
-        //     // Cuando llegue la ultima instruccion, meter jal para retroceder
-        //     if ( i == 9 ) begin
-        //         item_0.randomize() with {opcode==J_TYPE && imm_jal[20:10]==11'hfff ;};       
-        //         $display("\n(for JAL)\t\tInstruct #%d\t\tinstruct: %h\tOffset: %b   (bin)", i[15:0], item_0.full_inst, item_0.imm_jal);
-        //         // Transaccion JAL
-        //         start_item(item_0);
-        //         finish_item(item_0); 
-        //     end
-
-        //     // Instrucciones R
-        //     else begin
-        //         item_0.randomize() with {opcode==R_TYPE;};
-
-        //         start_item(item_0);
-        //         finish_item(item_0);
-
-        //         $display("\n(R type)\tInstruct #%d\t\tInstruction :%h\t", i[15:0], item_0.full_inst); 
-
-        //     end
-        // end
     endtask
 endclass
