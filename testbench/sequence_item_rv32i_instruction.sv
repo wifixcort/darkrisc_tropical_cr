93cda636f759ac6889cd3ce7ff22302734b68f78
dchavarrias <dennis.chavarriasoto@ucr.ac.cr>
2024-07-13 23:53:14 -0600

    Implementando jumps sequence

--- testbench/sequence_item_rv32i_instruction.sv
+++ testbench/sequence_item_rv32i_instruction.sv
@@ -24,13 +24,13 @@ class sequence_item_rv32i_instruction extends uvm_sequence_item;
   //*******************************************************
   `uvm_object_utils_begin(sequence_item_rv32i_instruction)
     `uvm_field_int (full_inst, UVM_DEFAULT)
-    // `uvm_field_int (opcode, UVM_DEFAULT)
-    // `uvm_field_int (rs1, UVM_DEFAULT)
-    // `uvm_field_int (rs2, UVM_DEFAULT)
-    // `uvm_field_int (rd, UVM_DEFAULT)
-    // `uvm_field_int (funct7, UVM_DEFAULT)
-    // `uvm_field_int (funct3, UVM_DEFAULT)
-    // `uvm_field_int (imm, UVM_DEFAULT)
+    `uvm_field_int (opcode, UVM_DEFAULT)
+    //`uvm_field_int (rs1, UVM_DEFAULT)
+    //`uvm_field_int (rs2, UVM_DEFAULT)
+    //`uvm_field_int (rd, UVM_DEFAULT)
+    //`uvm_field_int (funct7, UVM_DEFAULT)
+    //`uvm_field_int (funct3, UVM_DEFAULT)
+    `uvm_field_int (imm_jal, UVM_DEFAULT)
   `uvm_object_utils_end
 
 
@@ -224,6 +224,21 @@ class branch_aux_vars extends uvm_sequence_item; //TODO Revisar si se puede usar
 
   //*******************************************************
   `uvm_object_utils(branch_aux_vars)
+endclass
+
+class jump_aux_vars extends uvm_sequence_item; //TODO Revisar si se puede usar algo diferente a sequence.
+  function new(string name = "jump_aux_vars");
+    super.new(name);
+  endfunction
+
+  // random variables 
+  randc logic [3:0] num_instruction; //Cuantos grupos de 6 instrucciones se usaran?
+  randc logic [11:0] addi_imm; //Cuantos grupos de 6 instrucciones se usaran?
+  rand logic is_jalr;
+  constraint valid_rng_num_instruction {num_instruction>0; 15>num_instruction;}
+
+  //*******************************************************
+  `uvm_object_utils(jump_aux_vars)
 endclass
 
 //Excellent reference for conditional constraints:
